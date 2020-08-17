5e90b165babdcd21a9b360f6fde1c577bacdf12a
Francois Sommella <francois.sommella@gmail.com>
2020-08-17 18:13:14 +0200

    model facility

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_141944) do
+ActiveRecord::Schema.define(version: 2020_08_17_155104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "facilities", force: :cascade do |t|
+    t.string "address"
+    t.string "category"
+    t.boolean "available"
+    t.string "description"
+    t.integer "price"
+    t.integer "capacity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_facilities_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_08_17_141944) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "facilities", "users"
 end
