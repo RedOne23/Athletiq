b226b98044962b5caa16e573daf642f99f0764e1
Fadal EL ABDIOUI <elabdioui.fadal@gmail.com>
2020-08-18 12:12:01 +0200

    def new and create

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -8,9 +8,23 @@ class FacilitiesController < ApplicationController
   end
 
   def new
-
+    @facility = Facility.new
   end
 
   def create
+    @facility = Facility.new(facility_params)
+
+    if @facility.save
+      redirect_to facility_path(@facility), notice: "Your facility has been successfully created"
+    else 
+      render 'new'
+    end
   end
+
+  def facility_params
+    params.require(:facility).permit(:name, :address, :category, :description, :price, :capacity)
+  end
+
+
+
 end
