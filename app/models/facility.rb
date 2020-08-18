428f38a6ee36b5c592315125271191bdcd4da552
Fadal EL ABDIOUI <elabdioui.fadal@gmail.com>
2020-08-18 14:23:31 +0200

    Add dotenv - Protect my secret data in .env file

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -2,6 +2,6 @@ class Facility < ApplicationRecord
   belongs_to :user
   validates :address, presence: true, uniqueness: true
   validates :category, presence: true, uniqueness: true
-  validates :description, presence: true
+  validates :description, presence: true, inclusion: { in: ["Football", "Tennis", "Swimming pool", "Event"] }
   validates :name, presence: true
 end
